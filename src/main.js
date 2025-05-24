20a41e48052cbdf927b18e74dc2bbdfbfd21d1ff
AlexChindris01 <chindrisalex01@gmail.com>
2025-05-24 12:13:17 +0300

    First commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1 id="main-header">Weather App</h1>
+  
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+`;
+
