2d4a36d7965b9f81ec40205673e7900a99ff4fad
AlexChindris01 <chindrisalex01@gmail.com>
2025-05-24 19:05:04 +0300

    stuff

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1 id="main-header">Weather App</h1>
-  
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  <div id="main-card">
+    
+    <div style="display: grid; grid-template-columns: auto 250px">
+      <h1 id="main-header">Weather App</h1>
+      <input type="text" id="location-search" placeholder="Search for a location...">
+    </div>
+      <h1 class="large-header">Baia Mare</h1>
+<!--      <div style="padding: .1rem; margin: 0">-->
+      
+<!--      </div>-->
+      
+    
+    
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </div>
 `;
 
